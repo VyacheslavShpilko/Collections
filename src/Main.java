bf8728ca9bfadf26e17ddd6829e95b5fd1837b7d
VyacheslavShpilko <Spilkovv@gmail.com>
2023-03-30 10:36:59 +0300

    Start

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] mas1 = new int[]{5, 4, 3, 8, 15, 23, 20};
+        int[] mas2 = new int[]{3, 4, 9, 15, 8};
+
+    }
+}
+
+
